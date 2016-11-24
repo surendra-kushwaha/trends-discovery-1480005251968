4d189ec18b219f6fa51c373d3a2b6c3ce9cad69e
surendra.kushwaha <surendra.kushwaha@accenture.com>
2016-11-24 22:30:06 +0530

    discovery

--- src/example/nosql/AttachServlet.java
+++ src/example/nosql/AttachServlet.java
@@ -1,8 +1,6 @@
 package example.nosql;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -10,15 +8,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 
-import com.cloudant.client.api.Database;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 @WebServlet("/getTrends")
 @MultipartConfig()
@@ -35,22 +33,32 @@ public class AttachServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		trendsFinder();
+		
+		response.getWriter().print(trendsFinder());
+		
 		
 	}
 	
-	public void trendsFinder(){
+	public JsonObject trendsFinder(){
+		JsonObject responseJson=null;
 		try{			
 			String url = "http://api.walmartlabs.com/v1/search?apiKey=agevmwa5rhme979szegdj3v6&query=PHOTO%20SHADOW%20BOX%20TRAY&sort=customerRating&order=desc&numItems=5";
 			HttpClient client = HttpClientBuilder.create().build();
 			HttpGet request = new HttpGet(url);
 			request.addHeader("User-Agent", "");
 			HttpResponse response = client.execute(request);
+			
+			String result  = EntityUtils.toString(response.getEntity());
+			//JsonObject responseJson = (JsonObject)result;
+			JsonParser parser = new JsonParser();
+			responseJson = parser.parse(result).getAsJsonObject();
+			
 			System.out.println("Response status"+response.getStatusLine().getStatusCode());
 			System.out.println("Response from wallmart api:##"+response.toString());
 		}catch(Exception e){System.out.println(e);
 			//logger.error(e);
 		}
+		return responseJson;
 	}
 
 }
