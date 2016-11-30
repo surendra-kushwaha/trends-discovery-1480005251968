98437e2af9d3777f5deb71d84b6d43b92850bfb0
surendra.kushwaha <surendra.kushwaha@accenture.com>
2016-11-30 16:16:18 +0530

    semrush call

--- src/com/acit/trendsdiscovery/service/TrendsDiscoveryService.java
+++ src/com/acit/trendsdiscovery/service/TrendsDiscoveryService.java
@@ -1,6 +1,7 @@
 package com.acit.trendsdiscovery.service;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
@@ -21,6 +22,7 @@ import org.apache.wink.json4j.JSONObject;
 
 import com.acit.trendsdiscovery.dao.MetaKeywordsDAO;
 import com.acit.trendsdiscovery.model.MetaKeywords;
+import com.acit.trendsdiscovery.util.SemrushClient;
 import com.acit.trendsdiscovery.util.TopicAssociationGraphdb;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -50,12 +52,98 @@ public class TrendsDiscoveryService extends HttpServlet {
     	categories = topicAssocGraphdb.getCategories();
     	System.out.println("after category call ");*/
         //System.out.println("Categories in memcache not available  : "+categories);
-		trendsFinder();
+		trendsFinderSemrush();
 		//response.getWriter().print(trendsFinder()+"Categories ::"+categories);
         //response.getWriter().print(trendsFinder());
 		response.getWriter().print("Trends data uploaded to master data.");
 		
 		
+	}
+	
+	public void trendsFinderSemrush(){
+		JsonObject responseJson=null;
+		JsonElement je=null;
+		String trends=null;
+		String trendsRetrived[]=new String[5];
+		try{
+			
+			List<String> trendsData=metaKeywordsDAO.getTrendsDiscoveryData();
+			
+			System.out.println("trendsData iterator::"+trendsData.size());
+			Iterator<String> iterator = trendsData.iterator();
+			while (iterator.hasNext()) {
+					//System.out.println(iterator.next());
+					String mikSubclass=iterator.next();
+						
+					//String url = "http://api.walmartlabs.com/v1/search?apiKey=agevmwa5rhme979szegdj3v6&query=PHOTO%20SHADOW%20BOX%20TRAY&sort=customerRating&order=desc&numItems=5";
+					String url = "http://api.walmartlabs.com/v1/search?apiKey=agevmwa5rhme979szegdj3v6&query="+mikSubclass+"&sort=relevance&numItems=5";
+					HttpClient client = HttpClientBuilder.create().build();
+					HttpGet request = new HttpGet(url);
+					request.addHeader("User-Agent", "");
+					HttpResponse response = client.execute(request);
+					
+					String result  = EntityUtils.toString(response.getEntity());
+					//JsonObject responseJson = (JsonObject)result;
+					JsonParser parser = new JsonParser();
+					responseJson = parser.parse(result).getAsJsonObject();
+					JsonArray jsonArray=responseJson.getAsJsonArray("items");
+					
+					for(int i=0;i<jsonArray.size();i++){				
+						je=jsonArray.get(i);			
+						JsonParser parser1 = new JsonParser();
+						responseJson = parser1.parse(je.toString()).getAsJsonObject();
+						trends=responseJson.get("productUrl").getAsString();
+						
+						/*String trendsUrl=trends.substring(trends.lastIndexOf("?l=")+3,trends.lastIndexOf("47150667")-1);
+						
+						trends=trends.substring(trends.lastIndexOf("/")+1);
+						
+						System.out.println("trends count:"+i+"::"+trends);						
+						trendsRetrived[i]=trends;*/
+						
+						//Semrush call
+						System.out.println("getTrendsForAllKeywords Rest ");
+						SemrushClient semrushClient = new SemrushClient();						
+						//semrushClient.getTrendsFromSemrushAPI("",System.getenv("exportColumns"));						
+						//InputStreamReader in = semrushClient.getTrendsFromSemrushAPI("JEWELRY","Ph");
+						InputStreamReader in = semrushClient.getTrendsFromSemrushAPI("http://www.walmart.com/","Ph");
+						//InputStreamReader in = semrushClient.getTrendsFromSemrushAPI("Kids crafts",System.getenv("exportColumns"));
+						if(in != null){							
+							JSONObject keywordResult = semrushClient.processKeywordResults("http://www.walmart.com/", "Ph",in);
+							//JSONObject keywordResult = semrushClient.processKeywordResults("Kids crafts", System.getenv("exportColumns"),in);	
+							System.out.println("Keyword JSON : "+keywordResult);							
+							// Send the Semrush Keyword Result to Message Hub
+							//String topicName = System.getenv("topicName");
+							//postClient.postSemurshToMessageHub("topic=" + topicName + "&data=" + keywordResult);
+
+							//System.out.println("Keyword : "+ metaKeyword);
+							
+						}
+						
+						MetaKeywords metaKeyWords=new MetaKeywords();
+						//metaKeyWords.setKeywordName(trends);
+						metaKeyWords.setKeywordName("fall");
+						metaKeyWords.setActive("Y");
+						metaKeyWords.setLastUpdateDttm(new Date());
+						metaKeyWords.setModifiedBy("Raghav");
+						//Add to master table
+						//boolean semrushUpdate=metaKeywordsDAO.addSemurshMetaKeyword(metaKeyWords);
+						//boolean twitterUpdate=metaKeywordsDAO.addTwitterMetaKeyword(metaKeyWords);
+						
+						//System.out.println("update status twitter:"+twitterUpdate);
+						//System.out.println("update status semrush:"+semrushUpdate);
+						
+					}
+			
+			}
+			
+			
+			//System.out.println("Response status"+response.getStatusLine().getStatusCode());
+			//System.out.println("Response from wallmart api:##"+response.toString());
+		}catch(Exception e){System.out.println(e);
+			//logger.error(e);
+		}
+		//return trendsRetrived[0]+":"+trendsRetrived[1]+"::"+trends;
 	}
 	
 	public void trendsFinder(){
@@ -74,7 +162,7 @@ public class TrendsDiscoveryService extends HttpServlet {
 					String mikSubclass=iterator.next();
 						
 					//String url = "http://api.walmartlabs.com/v1/search?apiKey=agevmwa5rhme979szegdj3v6&query=PHOTO%20SHADOW%20BOX%20TRAY&sort=customerRating&order=desc&numItems=5";
-					String url = "http://api.walmartlabs.com/v1/search?apiKey=agevmwa5rhme979szegdj3v6&query="+mikSubclass+"&sort=customerRating&order=desc&numItems=5";
+					String url = "http://api.walmartlabs.com/v1/search?apiKey=agevmwa5rhme979szegdj3v6&query="+mikSubclass+"&sort=relevance&numItems=5";
 					HttpClient client = HttpClientBuilder.create().build();
 					HttpGet request = new HttpGet(url);
 					request.addHeader("User-Agent", "");
---
+++ src/com/acit/trendsdiscovery/util/SemrushClient.java
@@ -0,0 +1,136 @@
+/**
+ * 
+ */
+package com.acit.trendsdiscovery.util;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+
+import org.apache.wink.json4j.JSONArray;
+import org.apache.wink.json4j.JSONObject;
+
+/**
+ * @author indira
+ *
+ */
+
+public class SemrushClient {
+		
+	public InputStreamReader getTrendsFromSemrushAPI(String searchKeyword, String export_columns){
+	
+		System.out.println("In getTrendsFromSemrushAPI ..");
+		String keyword = searchKeyword.replace(" ", "+");
+		
+		System.out.println("Search Keyword : "+keyword);
+		
+		try{
+		URL url = new URL(System.getenv("semrushAPIEndpoint")
+				+ "?type=url_organic"
+				//+ "&phrase="+keyword //search keyword
+				+ "&url="+keyword //search keyword
+				+ "&key=" + System.getenv("semrushApiKey") //semrush API key(key=20230bcb09669464b97530a41e8e32c3)
+				+ "&display_limit=10"  //retrieve 10 records
+				+ "&export_columns="+export_columns  //colums to retrieve
+				+ "&database=us");  //currently limited to US DB
+		
+		HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+		InputStreamReader in = new InputStreamReader(
+				(urlConnection.getInputStream()));
+		
+		return in;
+		
+		}catch(Exception e ){
+			
+			System.out.println("Exception in getResultsFromSemrushAPI "+e);
+		}
+		
+		return null;
+	}
+	
+
+	public JSONObject processKeywordResults(String searchKeyword, String export_columns , InputStreamReader in){	
+		
+		System.out.println("In SemrushClient -processKeywordResults ");
+		try
+		{
+			String keyword = searchKeyword.replace(" ", "+");
+			
+			System.out.println("Search Keyword : "+keyword);
+			
+			String[] exportColumnsList= export_columns.split(",");
+			HashMap< Integer, String> columnIndex  = new HashMap<Integer, String>();
+			
+			for(int i=0; i<exportColumnsList.length; i++){
+				columnIndex.put(i,exportColumnsList[i]);
+			}
+
+			System.out.println("Resultant keywords from semrush :: ");
+			
+			JSONObject semrushResult = new JSONObject();
+			semrushResult = new JSONObject();
+			semrushResult.put("Topic", searchKeyword);
+			
+			JSONArray semrushKeyWordArray = new JSONArray();
+			
+			String result ="";
+			
+			BufferedReader br = new BufferedReader(in);
+			if((result = br.readLine()) != null) 
+				System.out.println("Header : " +result);
+				
+			while ((result = br.readLine()) != null) {
+				
+				JSONObject semrushKeyword = new JSONObject();
+
+				System.out.println(result);
+				
+				String [] keywords = result.split(";");
+				semrushKeyword = new JSONObject();
+
+				for(int i=0; i <keywords.length; i++){
+					
+					switch (columnIndex.get(i)) {
+					
+						case "Ph":
+									semrushKeyword.put("Keyword", keywords[i]);
+									break;
+						
+						case "Nq":
+									semrushKeyword.put("Search Volume", keywords[i]);
+									break;
+						case "Cp":
+									semrushKeyword.put("CPC", keywords[i]);
+									break;
+						case "Co":
+									semrushKeyword.put("Competition", keywords[i]);							
+									break;
+						case "Nr":
+									semrushKeyword.put("Number of Results", keywords[i]);							
+									break;
+						case "Td":
+									semrushKeyword.put("Trends", keywords[i]);							
+									break;
+						default:
+							break;
+					}
+				}
+				
+				semrushKeyWordArray.add(semrushKeyword);
+				}
+				
+				semrushResult.put("result", semrushKeyWordArray);
+				System.out.println("Keyword Result JSON : "+semrushResult);
+				
+				br.close();
+				
+				return semrushResult;
+			
+		}catch(Exception e ){
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
